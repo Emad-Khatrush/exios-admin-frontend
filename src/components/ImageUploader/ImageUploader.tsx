e430d01977f0325c2750ca76ea480498d002cf26
Emad Khatrush <qwe.emad@hotmail.com>
2025-07-08 13:31:31 +0300

    adding drag features in the uploader input

--- src/components/ImageUploader/ImageUploader.tsx
+++ src/components/ImageUploader/ImageUploader.tsx
@@ -19,19 +19,47 @@ type Props = {
 
 type State = {
   cameraDialog: boolean
+  isDragging: boolean
 }
 
 class ImageUploader extends Component<Props, State> {
 
   state: Readonly<State> = {
-    cameraDialog: false
+    cameraDialog: false,
+    isDragging: false
+  }
+
+  handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    this.setState({ isDragging: false });
+
+    const files = e.dataTransfer.files;
+    if (files && files.length > 0) {
+      // Create a synthetic event to match input onChange signature
+      const event = { target: { files } };
+      this.props.fileUploaderHandler(event);
+      e.dataTransfer.clearData();
+    }
+  }
+
+  handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({ isDragging: true });
+  }
+
+  handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({ isDragging: false });
   }
 
   render() {
     const { inputFileRef, fileUploaderHandler, previewFiles, id, files, required } = this.props;
-
     const uploadedFiles = !!files ? files : previewFiles;
-    
+
     return (
       <div>
         <CustomButton 
@@ -41,23 +69,33 @@ class ImageUploader extends Component<Props, State> {
         >
           Capture From Camera
         </CustomButton>
+
         <input 
           id={id} 
           multiple 
           className='d-none' 
-          onChange={fileUploaderHandler} 
+          onChange={fileUploaderHandler}
           ref={inputFileRef} 
           type="file" 
           required={required || false} 
         />
 
-        <div className='circle-border' onClick={() => inputFileRef.current.click()}>
+        <div 
+          className={`circle-border ${this.state.isDragging ? 'dragging' : ''}`}
+          onClick={() => inputFileRef.current.click()}
+          onDrop={this.handleDrop}
+          onDragOver={this.handleDragOver}
+          onDragLeave={this.handleDragLeave}
+        >
           <div className='upload-section'>
             <BiImageAdd className='icon' />
-            <span> Upload File </span>
+            <span> Upload File or Drag & Drop Here </span>
           </div>
         </div>
-        <p className='allow-format mb-2'> Allowed *.jpeg, *.jpg, *.png, *.pdf max size of 3.1 MB </p>
+
+        <p className='allow-format mb-2'>
+          Allowed *.jpeg, *.jpg, *.png, *.pdf max size of 3.1 MB
+        </p>
 
         <FilesPreviewers
           previewFiles={uploadedFiles}
@@ -65,16 +103,16 @@ class ImageUploader extends Component<Props, State> {
           deleteImage={this.props.deleteImage}
         />
 
-      <Dialog 
-        open={this.state.cameraDialog}
-        onClose={() => this.setState({ cameraDialog: false })}
-      >
-        <WebcamCapture 
-          fileUploaderHandler={fileUploaderHandler}
-          closeCamera={() => this.setState({ cameraDialog: false })}
-          packageId={id}
-        />
-      </Dialog>
+        <Dialog 
+          open={this.state.cameraDialog}
+          onClose={() => this.setState({ cameraDialog: false })}
+        >
+          <WebcamCapture 
+            fileUploaderHandler={fileUploaderHandler}
+            closeCamera={() => this.setState({ cameraDialog: false })}
+            packageId={id}
+          />
+        </Dialog>
       </div>
     )
   }
